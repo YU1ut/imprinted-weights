07eea46b1ac53f6d75ae33a72c6216e75b921860
YU1ut <imikushana@gmail.com>
2018-06-27 14:03:28 +0900

    change args

--- imprint_ft.py
+++ imprint_ft.py
@@ -27,7 +27,7 @@ parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.0001, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
